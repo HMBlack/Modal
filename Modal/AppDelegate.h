cbe64cb2d47fe7f1ea8220f03b30a2ffaec186c9
曾鹏浩 <Sunshine@cengpenghaodemacbook-pro.local>
2016-04-18 15:58:16 +0800

    Initial Commit

---
+++ Modal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Modal
+//
+//  Created by 曾鹏浩 on 16/4/18.
+//  Copyright © 2016年 曾鹏浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Modal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Modal
+//
+//  Created by 曾鹏浩 on 16/4/18.
+//  Copyright © 2016年 曾鹏浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
